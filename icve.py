1e3a1e0c85f6a8aff69d38be64c8c0f481bb0355
GeekLanyu <silence2021silence@163.com>
2022-12-29 17:29:44 +0800

    Update icve.py
    新版又不对劲了 缝缝补补还不如换新的 所以优化重写了一套

--- icve.py
+++ icve.py
@@ -1,7 +1,7 @@
 # -*- coding=utf-8 -*-
 """
-Time:        2022/12/28 22:40
-Version:     V 0.0.4
+Time:        2022/12/28 14:00
+Version:     V 0.0.5
 File:        icve.py
 Describe:    
 Author:      Lanyu
@@ -65,7 +65,6 @@ class OldMOOC:
 
         return final_task_list, widget_list
 
-
     def click(self, img, widget_list):
         verification = widget_list[1]
         verification_bar_0 = widget_list[2]
@@ -103,7 +102,6 @@ class OldMOOC:
                     pyautogui.scroll(-100)
                     print("寻找下一个任务")
 
-
     def main(self):
         print("程序已启动，2秒后开始操作")
         time.sleep(2)
@@ -128,85 +126,43 @@ class OldMOOC:
 
 class NewMOOC:
     def get_task(self):
-        workbook = xlrd.open_workbook("task_new.xls")
+        workbook = xlrd.open_workbook("task_new2.xls")
         worksheet = workbook.sheet_by_index(0)
-
-        # 控件列表
-        try:
-            widget_list = ["img/" + worksheet.col(0)[1].value, "img/" + worksheet.col(0)[3].value]
-        except IndexError:
-            print("表格第一列填写有误")
-            input("请输入任意内容后按回车键退出\n")
-            exit()
-
-        widget_list = ["img/" + worksheet.col(0)[1].value, "img/" + worksheet.col(0)[3].value]
-
-        # 任务列表
-        task_list1 = []
-        # 统计各列的任务数的列表
-        task_num = []
+        # 读取控件
+        widgets = [worksheet.col(0)[1].value, worksheet.col(0)[3].value]
+        # 读取项目
+        items = []
+        for j in worksheet.row_values(1):
+            items.append(j)
+        del items[0]
+        # 读取任务
+        tasks = []
         for i in range(worksheet.ncols - 1):
-            for j in range(len(worksheet.col(i)) - 1):
-                value = worksheet.col(i + 1)[j + 1].value
-                task_list1.append("img/" + value)
-                while "img/" in task_list1:
-                    task_list1.remove("img/")
-            task_num.append(len(task_list1))
-
-        # 将控件列表和任务列表合并
-        task_list2 = []
-        for i in task_list1:
-            task_list2.append(i)
-            for j in widget_list:
-                task_list2.append(j)
-
-        # 删除模块到第一个任务之间的控件
-        for i in range(len(task_num) - 1):
-            del task_list2[(task_num[i]) * 2 + (task_num[i]) + 1:(task_num[i]) * 2 + (task_num[i]) + 3]
-        del task_list2[1:3]
-
-        # 只含有任务的列表
-        task_list3 = []
-        for i in range(len(task_num)):
-            if task_num[i] == 2:
-                value = worksheet.col(i + 1)[2].value
-                task_list3.append("img/" + value)
-            elif task_num[i] > 2:
-                values = worksheet.col(i + 1)[2:task_num[i]]
-                for j in values:
-                    task_list3.append("img/" + j.value)
-            else:
-                print("表格第%d列填写有误" % (i + 1))
-                input("请输入任意内容后按回车键退出\n")
-                exit()
-
-        # 只含有项目的列表一式两份
-        task_list4 = []
-        task_list5 = []
-        for i in range(len(task_num)):
-            for j in range(task_num[i] - 1):
-                value = worksheet.col(i + 1)[1].value
-                task_list4.append("img/" + value)
-                task_list5.append("img/" + value)
-
-        # 添加折叠项防止误点
-        task_list6 = []
-        for i in task_list2:
-            if i == task_list2[3]:
-                task_list6.append(i)
-                task_list6.append(task_list3[0])
-                del task_list3[0]
-                task_list6.append(task_list4[0])
-                del task_list4[0]
-                task_list6.append(task_list5[0])
-                del task_list5[0]
-            else:
-                task_list6.append(i)
-
-        return task_list6, widget_list
+            tasks_ = []
+            for j in worksheet.col_values(i + 1):
+                tasks_.append(j)
+            tasks.append(tasks_)
+        # 去除任务中多余的数据
+        for i in tasks:
+            # 去除前两个元素
+            del i[0:2]
+            # 去除空元素
+            while "" in i:
+                i.remove("")
+        # 结合
+        combination = []
+        for i in range(len(tasks)):
+            for j in range(len(tasks[i])):
+                combination.append("img/" + items[i])
+                combination.append("img/" + tasks[i][j])
+                combination.append("img/" + widgets[0])
+                combination.append("img/" + widgets[1])
+                combination.append("img/" + tasks[i][j])
+                combination.append("img/" + items[i])
+        return combination, widgets
 
     def click(self, img, widget_list):
-        replay_key = widget_list[1]
+        play_key = widget_list[1]
         while True:
             if not os.path.exists(img):
                 print("找不到" + img + "文件")
@@ -218,7 +174,7 @@ class NewMOOC:
                 pyautogui.click(location.x, location.y, interval=0.2, duration=0.5, button="left")
                 break
             else:
-                if img == replay_key:
+                if img == play_key:
                     print("等待播放完毕")
                     time.sleep(1)
                 else:
@@ -254,13 +210,15 @@ def welcome():
     print("本程序已开源，源代码、打包可执行文件(.exe文件)、使用说明、配置教程都在GitHub和Gitee里，欢迎来Star和Fork，地址：")
     print("https://github.com/silence2021silence/")
     print("https://gitee.com/silence2021silence/")
-    print("技术支持与意见反馈可直接在仓库建issues或者关注微信公众号“geeklanyu”留言或者联系邮箱“silence2021silence@163.com”")
-    print("免责声明：本程序仅供学习、研究与娱乐使用，使用本程序违反相关法律或相关规章制度的与作者无关，禁止用于任何商业用途。")
+    print(
+        "技术支持与意见反馈可直接在仓库建issues或者关注微信公众号“geeklanyu”留言或者联系邮箱“silence2021silence@163.com”")
+    print(
+        "免责声明：本程序仅供学习、研究与娱乐使用，使用本程序违反相关法律或相关规章制度的与作者无关，禁止用于任何商业用途。")
     text = input("请输入“同意”或“不同意”后按回车键\n")
     if text != "同意":
         exit()
     else:
-        version = "v0.0.4"
+        version = "v0.0.5"
         print("当前版本为%s，正在检查更新..." % version)
         html = requests.get("https://gitee.com/silence2021silence/icve/blob/master/update.html").text
         soup = BeautifulSoup(html, 'lxml')
